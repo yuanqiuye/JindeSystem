896be8fdd74dfc45e59fb3ade81bf301890836c2
sean1227 <chenhsinyuan@gmail.com>
2019-07-16 13:59:55 +0800

    animate test

--- frontend/js/animate.js
+++ frontend/js/animate.js
@@ -1,14 +1,14 @@
-function animate(direction, url, data){
+function animate(direction, url, data) {
     const $start = $('#now');
-    const $end = $('.'+direction);
-    const $animation = $('#animation');
-    
+    const $end = $('.' + direction);
+    const $animation = $('#cssload-wrap3');
+
     $start.attr('id', '');
     $end.attr('id', 'now');
 
-    $start.fadeOut(500, function(){
+    $start.fadeOut(500, function() {
         $animation.fadeIn(500);
-       
+
     });
-    
+
 }
