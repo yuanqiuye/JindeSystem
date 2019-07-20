0143855e5f6d4fc31694da66886e764a113e8090
sean1227 <chenhsinyuan@gmail.com>
2019-07-20 18:10:41 +0800

    tets

--- backend/sql/mysql_connect.php
+++ backend/sql/mysql_connect.php
@@ -24,7 +24,25 @@ if (!$con)
   }
 
   if ($con -> select_db($db_name) === true){
-    if($con -> query($check_table) === false){
+    if($con -> query("create table if not exists jinde(
+      JID int not null AUTO_INCREMENT,
+      RID varchar(50) not null,
+      UID varchar(50) not null,
+      SID int not null,
+      finished bit DEFAULT 0,
+      applytime date not null,
+      PRIMARY KEY(JID)
+  );
+  
+  create table if not exists event(
+      EID int NOT NULL AUTO_INCREMENT,
+      JID int DEFAULT null,
+      teacher varchar(50) not null,
+      wantday int not null,
+      wanttime int not null,
+      finished bit DEFAULT 0,
+      PRIMARY KEY(EID)
+  );") === false){
       die("Unexpected error!");
     }
   }else{
