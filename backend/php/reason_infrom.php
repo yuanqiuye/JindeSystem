1d0550edca14be76a8de38c45cc8d77d3e5fd6c9
sean1227 <chenhsinyuan@gmail.com>
2019-07-21 11:25:56 +0800

    t

--- backend/php/reason_infrom.php
+++ backend/php/reason_infrom.php
@@ -23,6 +23,7 @@ if(decode_jwt($user, $jwt) === false || (int)decode_jwt($user, $jwt) < 1){
     $return["err"] = "登入逾時,不然就是你想亂來哈哈";
     echo json_encode($return);
 }else{
+    $con -> select_db("resourse");
     $reasonresult = $con -> query("SELECT * FROM reason");
     if($reasonresult === false){
         die($con->error);
