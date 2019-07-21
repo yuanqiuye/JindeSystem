77ea7b5776963727e5bdf861feb836340e993262
sean1227 <chenhsinyuan@gmail.com>
2019-07-21 08:51:52 +0800

    t

--- frontend/js/origin/callserver.js
+++ frontend/js/origin/callserver.js
@@ -1,71 +1,62 @@
 dowhat = new process();
 
 function callserver(url, send, callback) {
+    var dtd = $.Deferred();
     var request = JSON.stringify(send);
     var destination = "http://172.24.75.86:8787/JindeSystem/backend/php/";
 
     $.ajax({
         type: "POST",
-        async: false,
+        async: true,
         url: (destination + url),
         data: request,
         dataType: "json",
         cache: false,
-        success: function(get) {
-            console.log('my message' + get);
-            dowhat.freshdata(get);
-            if (get["err"] !== "") {
-                status_handler(get["err"], "err");
-            } else {
-                switch (get["type"]) {
-                    case "login":
-                        dowhat.login();
-                        break;
-                    case "reason_inform":
-                        dowhat.reason_inform();
-                        break;
-                    case "apply_jinde":
-                        dowhat.apply_jinde();
-                        break;
-                    case "apply_early_jinde":
-                        dowhat.apply_early_jinde();
-                        break;
-                    case "check_inform":
-                        dowhat.check_inform();
-                        break;
-                    case "check_early_jinde":
-                        dowhat.check_early_jinde();
-                        break;
-                    case "huge_check_jinde":
-                        dowhat.huge_check_jinde();
-                        break;
-                    case "output_jinde":
-                        dowhat.output_jinde();
-                        break;
-                    case "upload_jinde":
-                        dowhat.upload_jinde();
-                        break;
-                    case "student_apply_early_jinde":
-                        dowhat.student_apply_early_jinde();
-                        break;
-                }
+    }).done(function(get) {
+        console.log('my message' + get);
+        dowhat.freshdata(get);
+        if (get["err"] !== "") {
+            status_handler(get["err"], "err");
+        } else {
+            switch (get["type"]) {
+                case "login":
+                    dowhat.login();
+                    break;
+                case "reason_inform":
+                    dowhat.reason_inform();
+                    break;
+                case "apply_jinde":
+                    dowhat.apply_jinde();
+                    break;
+                case "apply_early_jinde":
+                    dowhat.apply_early_jinde();
+                    break;
+                case "check_inform":
+                    dowhat.check_inform();
+                    break;
+                case "check_early_jinde":
+                    dowhat.check_early_jinde();
+                    break;
+                case "huge_check_jinde":
+                    dowhat.huge_check_jinde();
+                    break;
+                case "output_jinde":
+                    dowhat.output_jinde();
+                    break;
+                case "upload_jinde":
+                    dowhat.upload_jinde();
+                    break;
+                case "student_apply_early_jinde":
+                    dowhat.student_apply_early_jinde();
+                    break;
             }
-        },
-        err: function(oResult, textStatus, errorThrown) {
-            animate_nondata("login", "403");
-        },
-        statusCode: {
-            404: function(response) {
-                animate_nondata("login", "403");
-            },
-            304: function(response) {
-                animate_nondata("login", "403");
+            if (typeof callback === 'function') {
+                callback();
             }
+            dtd.resolve(res);
         }
+    }).fail(function(jqXHR, textStatus, errorThrown) {
+        animate_nondata("login", "403");
+        dtd.reject(jqXHR, textStatus, errorThrown);
     });
-
-    if (typeof callback === 'function') {
-        callback();
-    }
-
 }
