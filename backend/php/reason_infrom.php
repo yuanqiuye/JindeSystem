44491e1c8a3b11757f992995f568be36f35148c1
sean1227 <chenhsinyuan@gmail.com>
2019-07-21 11:01:20 +0800

    t

--- backend/php/reason_infrom.php
+++ backend/php/reason_infrom.php
@@ -6,9 +6,9 @@ $data = json_decode(file_get_contents('php://input'), true);
 $user = $data["user"];
 $jwt = $data["jwt"];
 $return = array(
-    "type"=>"",
+    "type"=>"reason_inform",
     "err" => "",
-    "user" => "",
+    "user" => $user,
     "reason" => array(
         "name" => array(
 
@@ -20,5 +20,16 @@ $return = array(
 );
 
 if(decode_jwt($user, $jwt) === false){
-    
+    $return["err"] = "登入逾時,不然就是你想亂來哈哈";
+
+    echo json_encode($return);
+}else{
+    $reasonresult = $con -> query("SELECT * FROM reason");
+    while($reasonrow = mysqli_fetch_array($reasonresult)){
+        array_push($return["reason"]["name"], $reasonrow["description"]);
+        array_push($return["reason"]["RID"], $reasonrow["RID"]);
+    }
+
+    echo json_encode($return);
+
 }
