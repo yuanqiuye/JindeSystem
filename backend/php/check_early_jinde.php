98071bab13793f9ba32739811385f26ceb91b4a1
sean1227 <chenhsinyuan@gmail.com>
2019-07-23 03:25:09 +0800

    test

---
+++ backend/php/check_early_jinde.php
@@ -0,0 +1,31 @@
+<?php
+include ("../sql/mysql_connect.php");
+include ("jwt.php");
+
+$data = json_decode(file_get_contents('php://input'), true);
+$user = $data["user"];
+$jwt = $data["jwt"];
+$EID = $data["EID"];
+
+$return = array(
+    "type"=>"check_early_jinde",
+    "err" => "",
+    "user" => $user
+);
+
+if(decode_jwt($user, $jwt) === false || (int)decode_jwt($user, $jwt) < 1){
+    $return["err"] = "登入逾時,不然就是你想亂來哈哈";
+    echo json_encode($return);
+}else{
+    $con -> select_db($db_name);
+     $checklength = $EID.length;
+     for($i = 0; $i < $checklength; $i++){
+        $nowEID = $EID[$i];
+        $con -> query("UPDATE event SET finished = 1 WHERE EID = $nowEID");
+        $JIDr = $con ->query("SELECT JID FROM event WHERE EID = $nowEID");
+        $JIDrr = mysqli_fetch_array($JIDr);
+        $nowJID = $JIDrr["JID"];
+        $con ->  query("UPDATE jinde SET finished = 1 WHERE JID = $nowJID");
+     }
+     echo json_encode($return);
+}
