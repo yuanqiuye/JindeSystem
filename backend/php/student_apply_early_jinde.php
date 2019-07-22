e93e6ed4cf900630a1c402ef454da14db150fc6f
sean1227 <chenhsinyuan@gmail.com>
2019-07-22 20:33:34 +0800

    test

--- backend/php/student_apply_early_jinde.php
+++ backend/php/student_apply_early_jinde.php
@@ -7,12 +7,46 @@ $jwt = $data["jwt"];
 $SID = $data["user"];
 $number = $data["number"];
 $timeID = $data["timeID"];
+$nowday = date("w"); //Sunday, 0~6
 
 $return = array(
     "type"=> "apply_jinde",
     "err" => "",
-    "user" => $user,
+    "user" => $SID,
     "failed_times" => "",
     "success_location" => ""
 );
 
+if(decode_jwt($user, $jwt) === false || (int)decode_jwt($user, $jwt) !== 0){
+    $return["err"] = "登入逾時,不然就是你想亂來哈哈";
+    echo json_encode($return);
+}else{
+    $con -> select_db($db_name);
+
+    $sjp = $con -> prepare("SELECT JID FROM jinde WHERE SID = ? AND finished = 0");
+    $sjp = bind_param("s", $SID);
+    $sjp -> execute();
+    $sjpr = $sjp -> get_result();
+    $sjp -> close();
+
+    $officecheckp = $con -> prepare("SELECT office FROM event WHERE JID = null");
+    $officecheckp -> execute();
+    $officecheckpr = $officecheckp -> get_result();
+    $officecheckp -> close();
+    $officecheckprr = mysqli_fetch_array($officecheckpr);
+    $office = $officecheckprr["office"];
+
+    while($sjpr = mysqli_fetch_array($sjpr) && $number !== 0){
+        $ep = $con -> prepare("UPDATE event SET JID = ? WHERE JID = NULL AND office = ?");
+        if ($ep === false) break;
+        $notsuccess--;
+        $ep -> bind_param("ss", $sjpr["JID"], $office);
+        $ep -> execute();
+        $ep -> close();
+        $number--;
+    }
+    $return["failed_times"] = $number;
+    $return["success_location"] = $office;
+
+    echo json_encode($return);
+}
--- frontend/js/origin/process.js
+++ frontend/js/origin/process.js
@@ -103,7 +103,7 @@ class process {
     student_apply_early_jinde() {
         var spage = document.getElementById('success');
         if (this.data["failed_times"] !== 0) {
-            spage.innerHTML += this.data["failed"]["times"] + "隻申請失敗, 可能該處室名額為滿\n"
+            spage.innerHTML += this.data["failed_times"] + "隻申請失敗, 可能該處室名額為滿, \n或你申請太多隻了\n"
         }
         spage.innerHTML += "成功申請! 地點:" + this.data["success_location"];
         this.status = "studentsuccess";
