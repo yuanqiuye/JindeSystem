a87d64d35a2c2780d51983161e214c6c9f531011
sean1227 <chenhsinyuan@gmail.com>
2019-07-22 21:51:41 +0800

    tets

--- backend/php/student_apply_early_jinde.php
+++ backend/php/student_apply_early_jinde.php
@@ -54,8 +54,8 @@ if(decode_jwt($SID, $jwt) === false || (int)decode_jwt($SID, $jwt) !== 0){
         $office = $officecheckprr["office"];
         $EID = $officecheckprr["EID"];
 
-        print_r($sjprr);
-        print_r($officecheckprr);
+        print_r($sjpr);
+        print_r($officecheckpr);
 
         $ep = $con -> prepare("UPDATE event SET JID = ? WHERE EID = ? AND office = ? AND wantday = ? AND wanttime = ?");
         $ep -> bind_param("sssss", $JID, $EID, $office, $nowday, $timeID);
