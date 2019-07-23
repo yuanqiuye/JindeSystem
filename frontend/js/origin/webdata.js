3ae81c336abb423ebfcdd189d4bb2e65c495946c
morgan455092 <45846846+morgan455092@users.noreply.github.com>
2019-07-23 14:45:46 +0800

    1

--- frontend/js/origin/webdata.js
+++ frontend/js/origin/webdata.js
@@ -38,7 +38,7 @@ class webdata {
     }
 
     static student_apply_early_jinde() {
-        var timeID = $('input[name=student_time]:checked').get();
+        var timeID = $('input[name=student_time]:checked').val();
         var user = readcookie("user");
         var jwt = readcookie("jwt");
         var data = { "user": user, "jwt": jwt, "timeID": timeID};
