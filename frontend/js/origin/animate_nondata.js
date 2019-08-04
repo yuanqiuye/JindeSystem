cf9e4969e654cefffc98d156fa3753ae0c220968
sean1227 <chenhsinyuan@gmail.com>
2019-08-05 03:06:07 +0800

    ewqe

--- frontend/js/origin/animate_nondata.js
+++ frontend/js/origin/animate_nondata.js
@@ -16,12 +16,12 @@ function animate_nondata(direction, middle) {
         });
 
     }
-}
 
-$("#confirm").on("click", function() {
-    const $middle = $('.' + middle);
-    $middle.fadeOut(1000, function() {
-        $end.attr('id', 'now');
-        $end.fadeIn(400);
+    $("#confirm").on("click", function() {
+        const $middle = $('.' + middle);
+        $middle.fadeOut(1000, function() {
+            $end.attr('id', 'now');
+            $end.fadeIn(400);
+        });
     });
-})
+}
