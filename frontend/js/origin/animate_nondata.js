13e6d003a04f32b6316856f25e1888a187482d19
sean1227 <chenhsinyuan@gmail.com>
2019-08-05 04:08:43 +0800

    werwe

--- frontend/js/origin/animate_nondata.js
+++ frontend/js/origin/animate_nondata.js
@@ -6,7 +6,7 @@ function animate_nondata(direction, middle) {
     if (middle !== null) {
         $animation.fadeOut(100, function() {
             $middle.fadeIn(300, function() {
-                $("#confirm").on("click", function() {
+                $("button[id='confirm']").on("click", function() {
                     $middle.fadeOut(1000, function() {
                         $end.attr('id', 'now');
                         $end.fadeIn(400);
