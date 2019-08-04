68dc1027295a3ae9e134899a9c29e6d1116f9950
sean1227 <chenhsinyuan@gmail.com>
2019-08-05 03:29:54 +0800

    ew

--- frontend/js/origin/animate_nondata.js
+++ frontend/js/origin/animate_nondata.js
@@ -1,15 +1,16 @@
 function animate_nondata(direction, middle) {
-    var $start = $('#now');
-    $start.attr('id', '');
-    var $end = $('.' + direction);
-    $end.attr('id', 'now');
-    var $animation = $('.animation');
-    var $middle = $('.' + middle);
+    const $start = $('#now');
+    const $end = $('.' + direction);
+    const $animation = $('.animation');
+    const $middle = $('.' + middle)
     if (middle !== null) {
         $animation.fadeOut(100, function() {
             $middle.fadeIn(300);
-        });
+        })
     } else {
+        $start.attr('id', '');
+        $end.attr('id', 'now');
+
         $start.fadeOut(400, function() {
             $end.fadeIn(500);
         });
@@ -18,15 +19,16 @@ function animate_nondata(direction, middle) {
 
     $("#Sconfirm").on("click", function() {
         $middle.fadeOut(1000, function() {
+            $end.attr('id', 'now');
             $end.fadeIn(400);
         });
-    });
+    })
 
     $("#Fconfirm").on("click", function() {
         $middle.fadeOut(1000, function() {
+            $end.attr('id', 'now');
             $end.fadeIn(400);
         });
-    });
-
+    })
 
 }
