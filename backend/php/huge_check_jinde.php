7ad7768d605d6cf761574bdb8409e290cc375b01
morgan455092 <45846846+morgan455092@users.noreply.github.com>
2019-08-06 21:01:49 +0800

    Update huge_check_jinde.php

--- backend/php/huge_check_jinde.php
+++ backend/php/huge_check_jinde.php
@@ -33,7 +33,7 @@ if(decode_jwt($user, $jwt) === false || (int)decode_jwt($user, $jwt) < 3){
             $nowtimes = $times[$i];
             $con -> select_db($db_name);
             $result = $con -> query("SELECT JID FROM jinde WHERE (SID = $nowSID && finished = 0)");
-            $JIDr = $result -> fetch_array();
+            $JIDr = mysqli_fetch_array($result,MYSQLI_NUM);
             $return["result"] = $JIDr;
             $JIDlength = sizeof($JIDr);
             for($ii = 0; $ii < $nowtimes && $JIDlength > 0 ; $ii++){
