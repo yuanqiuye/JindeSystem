3ea6b23a66f30df889799b4e9c3d698ddb5a4e4b
morgan455092 <45846846+morgan455092@users.noreply.github.com>
2019-08-06 20:29:03 +0800

    Update huge_check_jinde.php

--- backend/php/huge_check_jinde.php
+++ backend/php/huge_check_jinde.php
@@ -31,15 +31,15 @@ if(decode_jwt($user, $jwt) === false || (int)decode_jwt($user, $jwt) < 3){
         }else{
             $nowtimes = $times[$i];
             $con -> select_db($db_name);
-            $result = $con -> query("SELECT JID FROM jinde WHERE SID = $nowSID && finished != 1");
+            $result = $con -> query("SELECT JID FROM jinde WHERE (SID = $nowSID && finished = 0)");
             $JIDr = $result -> fetch_array();
             $JIDlength = sizeof($JIDr);
             for($ii = 0; $ii < $nowtimes && $JIDlength > 0 ; $ii++){
                 $ar = $con -> prepare("UPDATE jinde SET finished = 1 where JID = ?");
                 $ar -> bind_param("s", $JIDr[$ii]);
                 $ar -> execute();
+                $ar -> close();
             }
-            $ar -> close();
         }
         echo json_encode($return);
     }
