f370fd68581dd443db7a0012f97664b8ae3594c4
morgan455092 <45846846+morgan455092@users.noreply.github.com>
2019-08-06 19:31:02 +0800

    Update process.js

--- frontend/js/origin/process.js
+++ frontend/js/origin/process.js
@@ -153,6 +153,8 @@ class process {
     }
 
     huge_check_jinde() {
+        var spage = document.getElementById('success');
+        spage.innerHTML = "";
         if (this.data["failed_SID"] !== 0) {
             spage.innerHTML += this.data["failed_SID"] + "消除失敗, 可能查無此學號, 或根本沒那麼多隻可以消XDD\n"
         }
