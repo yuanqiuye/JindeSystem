a28a2843cd283ae05d192b3682226faca0f364e5
morgan455092 <45846846+morgan455092@users.noreply.github.com>
2019-08-08 14:33:21 +0800

    Update err_animate_nondata.js

--- frontend/js/origin/err_animate_nondata.js
+++ frontend/js/origin/err_animate_nondata.js
@@ -1,1 +1,27 @@
+function animate_nondata(direction, middle) {
+    const $start = $('#now');
+    const $end = $('.' + direction);
+    const $animation = $('.animation');
+    const $middle = $('.' + middle)
+    if (middle !== null) {
+        $animation.fadeOut(100, function() {
+            $middle.fadeIn(100, function() {
+                $("button[id='confirm2']").on("click", function() {
+                $middle.fadeOut(100, function() {
+                    $end.attr('id', 'now');
+                    $end.fadeIn(100);
+                });
+                })
+            });
+        })
+    } else {
+        $start.attr('id', '');
+        $end.attr('id', 'now');
 
+        $start.fadeOut(400, function() {
+            $end.fadeIn(500);
+        });
+
+    }
+
+}
