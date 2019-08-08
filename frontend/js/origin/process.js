dc2267e81771192ca63ed90b561e1750e7f45b40
morgan455092 <45846846+morgan455092@users.noreply.github.com>
2019-08-08 12:24:43 +0800

    Update process.js

--- frontend/js/origin/process.js
+++ frontend/js/origin/process.js
@@ -159,8 +159,9 @@ class process {
             for(var i=0;i<this.data["failed_SID"].length;i++){
                 spage.innerHTML += this.data["failed_SID"][i] + "消除失敗, 可能查無此學號, 或根本沒那麼多隻可以消XDD\n"
             }
+            this.status = "studentsuccess";
         }
-        this.status = "applysuccess";
+        else {this.status = "applysuccess";}
         this.direction = "selection";
         
     }
