e604f50b807d9f673c569ce37179b946582fb346
morgan455092 <45846846+morgan455092@users.noreply.github.com>
2019-08-08 12:47:17 +0800

    Update process.js

--- frontend/js/origin/process.js
+++ frontend/js/origin/process.js
@@ -39,12 +39,18 @@ class process {
                 break;
             case "teacher":
                 var level = this.data["level"];
-                if(level == 3){}
-                else if (level == 2) {
+                if(level == 3){
+                    document.getElementById('high').setAttribute("style", "display:block");
+                    document.getElementById('middle').setAttribute("style", "display:block");
+                    document.getElementById('low').setAttribute("style", "display:block");
+                }else if (level == 2) {
                     document.getElementById('high').setAttribute("style", "display:none");
+                    document.getElementById('middle').setAttribute("style", "display:block");
+                    document.getElementById('low').setAttribute("style", "display:block");
                 } else if (level == 1) {
                     document.getElementById('high').setAttribute("style", "display:none");
                     document.getElementById('middle').setAttribute("style", "display:none");
+                    document.getElementById('low').setAttribute("style", "display:block");
                 }
                 document.cookie = "user=" + this.data["user"];
                 document.cookie = "jwt=" + this.data["jwt"];
