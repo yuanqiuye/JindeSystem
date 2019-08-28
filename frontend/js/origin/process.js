80ab71cc82cc01caa716d463c23957cef7675d7e
morgan455092 <45846846+morgan455092@users.noreply.github.com>
2019-08-28 15:02:07 +0800

    Update process.js

--- frontend/js/origin/process.js
+++ frontend/js/origin/process.js
@@ -57,6 +57,7 @@ class process {
                 document.cookie = "user=" + this.data["user"];
                 document.cookie = "jwt=" + this.data["jwt"];
                 document.getElementById('logined').setAttribute("style", "-1");
+                document.getElementById('show_user').append(this.data["user"]);
 
                 this.direction = "selection";
                 this.status = "loginsuccess";
