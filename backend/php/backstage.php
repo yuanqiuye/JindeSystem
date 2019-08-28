2d3cc28f731bc781b731dafcf5d9603ac585d6a0
morgan455092 <45846846+morgan455092@users.noreply.github.com>
2019-08-28 18:05:59 +0800

    Update backstage.php

--- backend/php/backstage.php
+++ backend/php/backstage.php
@@ -3,10 +3,10 @@ ini_set('display_errors','off');
 include ("../sql/mysql_connect.php");
 include ("jwt.php");
 $data = json_decode(file_get_contents('php://input'), true);
-/*if(decode_jwt($user, $jwt) === false || (int)decode_jwt($user, $jwt) < 1){
+if(decode_jwt($user, $jwt) === false || (int)decode_jwt($user, $jwt) < 3){
     $return["err"] = "登入逾時,不然就是你想亂來哈哈";
     echo json_encode($return);
-}else{*/
+}else{
 	echo '<form method="post">
 			搜尋<br><br>
 			學號<input type="text" name="SID"/>
@@ -54,5 +54,5 @@ $data = json_decode(file_get_contents('php://input'), true);
     $ar -> close();
     $arrow = mysqli_fetch_array($arr);
 	echo $arrow;
-//}
+}
 ?>
