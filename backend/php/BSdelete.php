6fe8ecf6f88293c26196de274bf782d80462366a
morgan455092 <45846846+morgan455092@users.noreply.github.com>
2019-08-29 14:50:18 +0800

    Update BSdelete.php

--- backend/php/BSdelete.php
+++ backend/php/BSdelete.php
@@ -1,12 +1,11 @@
 <?php
-//ini_set('display_errors','off');
+ini_set('display_errors','off');
 include ("../sql/mysql_connect.php");
 include ("jwt.php");
-/*$data = json_decode(file_get_contents('php://input'), true);
+$data = json_decode(file_get_contents('php://input'), true);
 if(decode_jwt($user, $jwt) === false || (int)decode_jwt($user, $jwt) < 3){
-    $return["err"] = "登入逾時,不然就是你想亂來哈哈";
-    echo json_encode($return);
-}else{*/
+    header("Location:http://jindesys.nctu.me");
+}else{
 	$return="";
 	$JID=$_GET["id"];
 	$con -> select_db($db_name);
