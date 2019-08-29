a60328b25cd0a12c64ff16ccec9836a65def8942
morgan455092 <45846846+morgan455092@users.noreply.github.com>
2019-08-29 15:07:09 +0800

    Update backstage.php

--- backend/php/backstage.php
+++ backend/php/backstage.php
@@ -2,7 +2,7 @@
 ini_set('display_errors','off');
 include ("../sql/mysql_connect.php");
 include ("jwt.php");
-$user=htmlspecialchars($_COOKIE["user"]);
+$user=$HTTP_COOKIE_VARS[""];
 echo $user;
 /*$data = json_decode(file_get_contents('php://input'), true);
 if(decode_jwt($user, $jwt) === false || (int)decode_jwt($user, $jwt) < 3){
