d338da870274108491a25421e19a872606d13c01
morgan455092 <45846846+morgan455092@users.noreply.github.com>
2019-08-29 13:56:24 +0800

    Update backstageSH.php

--- backend/php/backstageSH.php
+++ backend/php/backstageSH.php
@@ -11,7 +11,7 @@ if(decode_jwt($user, $jwt) === false || (int)decode_jwt($user, $jwt) < 3){
 	
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>
 		<script>
-		$(document).ready(function(){
+		$(document).ready(
 			function delete(id) {
 				  $.ajax({
 				    type: "POST",
@@ -22,8 +22,8 @@ if(decode_jwt($user, $jwt) === false || (int)decode_jwt($user, $jwt) < 3){
 				  }).done(function( msg ) {
 					location.reload();
 				  }).fail(function(){alert("failed");});
-			};
-	});
+			
+		});
 		</script>
 		</head>
 	';
