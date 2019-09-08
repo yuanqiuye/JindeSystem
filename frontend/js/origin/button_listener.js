5285d78af0157e8ff7c1328945bdc27e0f9938dd
sean1227 <chenhsinyuan@gmail.com>
2019-09-08 22:46:57 +0800

    fix

--- frontend/js/origin/button_listener.js
+++ frontend/js/origin/button_listener.js
@@ -118,7 +118,7 @@ $(document).ready(function() {
         var Today = new Date();
 
         if (Today.getDay() > 3 || Today.getDay() < 1 || Today.getHours() <= 7 || Today.getHours() >= 11) {
-            alert("已超時，請在當日11:00以前登記完畢");
+            alert("已超時，請在 (一)~(三) 7:00~11:00以前登記完畢");
         } else {
 
             webdata.student_apply_early_jinde();
