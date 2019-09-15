9972f828c6a58407f67a90160ac6a9793d826aa2
sean1227 <chenhsinyuan@gmail.com>
2019-09-15 11:30:59 +0800

    fix fetch_array

--- backend/sql/mysql_connect.php
+++ backend/sql/mysql_connect.php
@@ -29,23 +29,27 @@ if (!$con){
 }
 
 function check_access_flag($SID){
+
   global $date_array, $con;
+
   $sjinde = $con -> prepare("SELECT JID FROM jinde WHERE SID = ? AND finished = 0 
   AND NOT EXISTS (SELECT * FROM jinde WHERE jinde.RID = 'g1')
   AND applytime BETWEEN '$date_array[3]' AND '$date_array[4]'");
+  
   $sjinde->bind_param("s", $SID);
   $sjinde->execute();
   $sjinder = $sjinde->get_result();
   $sjinde->close();
+  $jinde_number = mysqli_fetch_array($sjinder);
 
-  if(sizeof($sjinder) >= 3){
-      for($i = 0; $i < sizeof($sjinder); $i++){
-          $nowJID = $sjinder[$i];
+  if(sizeof($jinde_number) >= 3){
+      for($i = 0; $i < sizeof($jinde_number); $i++){
+          $nowJID = $jinde_number[$i];
           $con ->  query("UPDATE jinde SET access_flag = 0 WHERE JID = $nowJID");
       }
   }else{
-      for($i = 0; $i < sizeof($sjinder); $i++){
-          $nowJID = $sjinder[$i];
+      for($i = 0; $i < sizeof($jinde_number); $i++){
+          $nowJID = $jinde_number[$i];
           $con ->  query("UPDATE jinde SET access_flag = 1 WHERE JID = $nowJID");
       }
   }
