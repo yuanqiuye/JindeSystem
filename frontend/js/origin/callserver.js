a89eecc5a3fd5dff04b2d743509f7d4e88aff2f9
unknown <chenhsinyuan@gmail.com>
2019-09-22 21:45:22 +0800

    test

--- frontend/js/origin/callserver.js
+++ frontend/js/origin/callserver.js
@@ -18,48 +18,50 @@ function callserver(url, send, callback) {
         dowhat.freshdata(get);
         if (get["err"] == "查無此班級座號！") {
             status_handler(get["err"], "backstage_search");
-        } else if(get["err"] !== ""){
-            status_handler(get["err"], "login");
-        }else{
-            switch (get["type"]) {
-                case "login":
-                    dowhat.login();
-                    break;
-                case "reason_inform":
-                    dowhat.reason_inform();
-                    break;
-                case "apply_jinde":
-                    dowhat.apply_jinde();
-                    break;
-                case "apply_early_jinde":
-                    dowhat.apply_early_jinde();
-                    break;
-                case "check_inform":
-                    dowhat.check_inform();
-                    break;
-                case "check_early_jinde":
-                    dowhat.check_early_jinde();
-                    break;
-                case "backstage_search":
-                    dowhat.backstage_search();
-                    break;
-                case "huge_check_jinde":
-                    dowhat.huge_check_jinde();
-                    break;
-                case "output_jinde":
-                    dowhat.output_jinde();
-                    break;
-                case "upload_jinde":
-                    dowhat.upload_jinde();
-                    break;
-                case "student_apply_early_jinde":
-                    dowhat.student_apply_early_jinde();
-                    break;
+        } else {
+            if(get["err"] !== ""){
+                status_handler(get["err"], "login");
+            }else{
+                switch (get["type"]) {
+                    case "login":
+                        dowhat.login();
+                        break;
+                    case "reason_inform":
+                        dowhat.reason_inform();
+                        break;
+                    case "apply_jinde":
+                        dowhat.apply_jinde();
+                        break;
+                    case "apply_early_jinde":
+                        dowhat.apply_early_jinde();
+                        break;
+                    case "check_inform":
+                        dowhat.check_inform();
+                        break;
+                    case "check_early_jinde":
+                        dowhat.check_early_jinde();
+                        break;
+                    case "backstage_search":
+                        dowhat.backstage_search();
+                        break;
+                    case "huge_check_jinde":
+                        dowhat.huge_check_jinde();
+                        break;
+                    case "output_jinde":
+                        dowhat.output_jinde();
+                        break;
+                    case "upload_jinde":
+                        dowhat.upload_jinde();
+                        break;
+                    case "student_apply_early_jinde":
+                        dowhat.student_apply_early_jinde();
+                        break;
+                }
+                if (typeof callback === 'function') {
+                    callback();
+                }
+                dtd.resolve(data);
             }
-            if (typeof callback === 'function') {
-                callback();
-            }
-            dtd.resolve(data);
         }
     }).fail(function(jqXHR, textStatus, errorThrown) {
         animate_nondata("login", "403");
