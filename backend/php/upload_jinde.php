927ebb40f7cb25b11167534f53f902cb1bcc1b64
unknown <chenhsinyuan@gmail.com>
2019-09-29 08:25:31 +0800

    var_dump test

--- backend/php/upload_jinde.php
+++ backend/php/upload_jinde.php
@@ -18,6 +18,6 @@ if(decode_jwt($user, $jwt) === false || (int)decode_jwt($user, $jwt) < 3){
     $return["err"] = "登入逾時,不然就是你想亂來哈哈";
     echo json_encode($return);
 }else{
-    $return["file"] = $file;
+    $return["file"] = var_dump($file);
     echo json_encode($return);
 }
