3ae2887c00a26a02dc37a2865fd737125fe20d30
unknown <chenhsinyuan@gmail.com>
2019-09-30 16:43:13 +0800

    test

--- backend/php/upload_jinde.php
+++ backend/php/upload_jinde.php
@@ -19,11 +19,5 @@ $return = array(
 );
 
 $file_data = base64_decode($file);
-file_put_contents("/home/jindesys/Desktop/python/late.csv", $file_data);
-$path="python ~/Desktop/python/late.py "; //需要注意的是：末尾要加一個空格
-$result = passthru($path.$params);
-if($result == "complete"){
-    echo "true";
-}else{
-    echo $result;
-}
+file_put_contents("late.csv", $file_data);
+
